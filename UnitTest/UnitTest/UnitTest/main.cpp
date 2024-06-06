5b2d890e0b014e165ef0d1345b0e662c6bc04c56
Yuhei Akama <yuhei-akama@atlus.co.jp>
2024-06-06 14:02:26 +0900

    WIP: studying Simplex noise

--- UnitTest/UnitTest/UnitTest/main.cpp
+++ UnitTest/UnitTest/UnitTest/main.cpp
@@ -47,6 +47,42 @@ std::pair<T, T> Unskew(T i, T j)
     return { x, y };
 }
 
+template<class T>
+std::tuple<T, T, T> SimplexInterpolate(T x, T y, bool& isInTrianle)
+{
+    const T F2 = (std::sqrt(3) - 1) / static_cast<T>(2);
+
+    T s = (x + y) * F2;
+    auto i = std::floor(x + s);
+    auto j = std::floor(y + s);
+
+    const T G2 = (3 - std::sqrt(3)) / static_cast<T>(6);
+
+    T t = (i + j) * G2;
+    auto X0 = i - t;
+    auto Y0 = j - t;
+
+    T x0 = x - X0;
+    T y0 = y - Y0;
+
+    std::int32_t i1 = 0, j1 = 0;
+    (x0 > y0) ? i1 = 1 : j1 = 1;
+    if (x+s < y+s)
+        isInTrianle = false;
+
+    auto x1 = (x0 - i1) + G2;
+    auto y1 = (y0 - j1) + G2;
+    auto x2 = (x0 - 1) + 2 * G2;
+    auto y2 = (y0 - 1) + 2 * G2;
+
+    constexpr T c = 0.5;
+    T t0 = c - x0 * x0 - y0 * y0;
+    T t1 = c - x1 * x1 - y1 * y1;
+    T t2 = c - x2 * x2 - y2 * y2;
+
+    return { x + s, y + s, t1};
+}
+
 TEST_SUITE("Util")
 {
     TEST_CASE("Random table")
@@ -55,7 +91,7 @@ TEST_SUITE("Util")
         std::mt19937 gen(seed);
 
         constexpr auto N = 256;
-        auto table = mlnoise::detail::RandomTable<std::float_t, N>(gen);
+        auto table = mlnoise::detail::RandomTable_11<std::float_t, N>(gen);
 
         for (auto itr = table.begin(); itr < table.begin() + N; ++itr)
         {
@@ -97,17 +133,38 @@ TEST_SUITE("Plot")
     {
         std::ofstream skew_ofs  ("./plotData/skew.dat");
         std::ofstream unskew_ofs("./plotData/unskew.dat");
+        std::ofstream interp_ofs("./plotData/simplex_interp.dat");
 
         for (auto y = 0; y < 32; ++y)
         {
+            bool canNewLine = false;
             for (auto x = 0; x < 32; ++x)
             {
-                auto skewed = Skew(static_cast<std::float_t>(x), static_cast<std::float_t>(y));
+                auto skewed = Skew(static_cast<std::float_t>(x/32.f), static_cast<std::float_t>(y/32.f));
                 auto unskewed = Unskew(static_cast<std::float_t>(skewed.first), static_cast<std::float_t>(skewed.second));
 
                 skew_ofs << skewed.first << " " << skewed.second << std::endl;
                 unskew_ofs << unskewed.first << " " << unskewed.second << std::endl;
+
+                bool isInLowerTriangle = true;
+                auto interp = SimplexInterpolate(static_cast<std::float_t>(x/32.f), static_cast<std::float_t>(y/32.f), isInLowerTriangle);
+                if (isInLowerTriangle)
+                {
+                    interp_ofs << std::get<0>(interp) << " " << std::get<1>(interp) << " " << std::get<2>(interp) << std::endl;
+                    canNewLine = true;
+                }
+                else
+                {
+                    if (canNewLine)
+                    {
+                        interp_ofs << std::endl;
+                        canNewLine = false;
+                    }
+                }
             }
+
+            if(canNewLine)
+                interp_ofs << std::endl;
         }
     }
 
---
+++ UnitTest/UnitTest/UnitTest/plotData/check_simplex_interp.g
@@ -0,0 +1,5 @@
+#plot "simplex_interp.dat"
+set pm3d map interpolate 0,0
+set cbrange[0:1]
+splot "simplex_interp.dat"
+pause -1 "hit Enter key"
